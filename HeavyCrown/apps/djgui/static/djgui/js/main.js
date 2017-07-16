bf2e9eeaa9969d61e2b7c9f23e86b2f2cd19700b
Ronin Dusette <ronin@Ronins-iMac.local>
2017-07-16 13:18:46 -0700

    work work work. Got basic layout of site done. Will likely need some fine-tuning, but it's responsive. Most of the work will likely need to be done on the back end.

--- HeavyCrown/apps/djgui/static/djgui/js/main.js
+++ HeavyCrown/apps/djgui/static/djgui/js/main.js
@@ -1,1 +1,18 @@
 
+$(document).ready(function(){
+    $("#social_link_facebook").click(function(){
+        window.location.href='https://www.facebook.com/HeavyCrownMedia/'
+    });
+});
+
+$(document).ready(function(){
+    $("#social_link_twitter").click(function(){
+        window.location.href='#'
+    });
+});
+
+$(document).ready(function(){
+    $("#social_link_linkedin").click(function(){
+        window.location.href='#'
+    });
+});
