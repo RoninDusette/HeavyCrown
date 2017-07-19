88a3be8b0229a81f294cf6e51392c94338e014c3
Ronin Dusette <ronin@Ronins-iMac.local>
2017-07-18 22:50:55 -0700

    added js to links to scroll the page and tweaked the mobile stuff a bit

--- HeavyCrown/apps/djgui/static/djgui/js/main.js
+++ HeavyCrown/apps/djgui/static/djgui/js/main.js
@@ -16,3 +16,21 @@ $(document).ready(function(){
         window.location.href='#'
     });
 });
+
+$(".home-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".home").offset().top
+    }, 500);
+});
+
+$(".work-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".work").offset().top
+    }, 500);
+});
+
+$(".contact-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".contact").offset().top
+    }, 500);
+});
